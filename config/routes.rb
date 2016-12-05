a975a6a2bf4986e41c96ec8eba8b1b7959cc11e0
Amira Al Baya <abaya@qatar.cmu.edu>
2016-12-05 10:13:25 +0200

    bye

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   resources :mail_items
   resources :locations
   resources :users
+      get :be_admin, on: :member
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
